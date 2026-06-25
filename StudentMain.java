0e7e91cce1f2aa1fdbd910dde91f0777b5bdf07d
sk471612 <daejung1623@dge.go.kr>
2026-06-25 15:09:01 +0900

    Add files via upload

--- StudentMain.java
+++ StudentMain.java
@@ -1,46 +1,48 @@
 package jump2java;
 import java.util.Scanner;
 
-class Student{
+
+class Student {
 	String name;
-	double score;
+	int score;
+
 	
-	Student(String name, double score){
+	Student(String name, int score) {
 		this.name = name;
 		this.score = score;
 	}
-	
 	void printInfo() {
-		System.out.println(name+", "+score);
+		System.out.println(name + " 점수:"+ score);
 	}
-	
-	String getGrade() {
-		if (score >= 90) {
-			return "A";
-		} else if (score >= 80) {
-			return "B";
-		} else if (score >= 70) {
-			return "C";
-		} else if (score >= 60) {
-			return "D";
-		} else {
-			return "F";
+	public String getGrade() {
+		if(score >= 90) return "A";
+		else if(score >= 80) return "B";
+		else if(score >= 70) return "C";
+		else if(score >= 60) return "D";
+		else return"F";
+		
+		
+	 }
+			
 		}
-	}
-}
+		
+	
 
 public class StudentMain {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("이름, 정수를 입력 하시오.");
+		System.out.print("학생 이름: ");
 		String name = sc.next();
-		double score = sc.nextDouble();
-		
-		Student s1 = new Student(name, score);
+		System.out.print("점수: ");
+		int score = sc.nextInt();
+		Student s1 = new Student (name,score);
 		s1.printInfo();
-		System.out.println(s1.getGrade() + "등급 입니다.");
+		System.out.println(s1.getGrade() + "등급입니다");
+		sc.close();
+		
 		
 
 	}
-
 }
+
